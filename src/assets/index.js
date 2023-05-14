18d315c6c0029cbc04991e122d26f41a78d475b6
Murtaza <murtazahatimr@icloud.com>
2023-05-15 00:43:25 +0530

    Add experience section.

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import gs from "./company/gs.png";
+import monash from "./company/monash.png";
+import photobook from "./company/photobook.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  gs,
+  monash,
+  photobook,
   carrent,
   jobit,
   tripguide,
