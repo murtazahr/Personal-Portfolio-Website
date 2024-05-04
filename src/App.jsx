ba3075e3884fe25bf7517179ed54c296d086bb19
Murtaza Rangwala <murtazahatimr@icloud.com>
2024-05-04 10:37:33 +0530

    Get real time speed insights and analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
 import Research from "./pages/Research.jsx";
@@ -7,6 +8,8 @@ import Research from "./pages/Research.jsx";
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
+      <Analytics />
+      <SpeedInsights />
       <Router>
         <Navbar />
         <Routes>
